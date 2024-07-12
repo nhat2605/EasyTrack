2055efcbb86fdd8e80c2b9d807572abcf2619cca
Nhat <lale6280@uni.sydney.edu.au>
2024-07-13 00:30:01 +1000

    users and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,16 @@ import { Component } from '@angular/core';
 
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
+import { FooterComponent } from "./footer/footer.component";
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent,UserComponent],
+  imports: [HeaderComponent, UserComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  users = DUMMY_USERS;
+}
